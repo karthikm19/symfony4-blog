e0e93eaaec598d90efa8e0c0bae94e4a02140bc9
Nataraj <karthik.nataraj@bt.com>
2019-08-29 14:45:04 +0530

    added bootstrap look & feel with static homepage and view article page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-class ArticleController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArticleController extends AbstractController
 {
 	/**
 	 * @Route("/")
 	 */
 	public function homepage()
 	{
-		return new Response('Hey, I am working!');
+		return $this->render('article/homepage.html.twig');
+	}
+
+	/**
+	 * @Route("/article/{title}")
+	 */
+	public function show($title)
+	{
+		return $this->render('article/show.html.twig', [
+			'title' => str_replace("-", " ", $title)
+		]);
 	}
 }
